28972b60038fcb6f73da277f934c58f2236a5ab0
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-10-30 17:13:14 +0200

    definitivo btc

--- Tarea8bitcoin.py
+++ Tarea8bitcoin.py
@@ -1,5 +1,5 @@
 bitcoin_amount = 0.5 #cuanto bitcoin tengo
-bitcoin_value_euros = 40000 #el valor de 1 bitcoin en euros
+bitcoin_value_euros = 40000 #el valor actual de 1 bitcoin en euros
 
 def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
     euros_value = bitcoin_amount * bitcoin_value_euros
