0f607a3392e43fdf3e549ae143c44fd41444d77d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-10-30 21:17:36 +0200

    diagrama

--- Tarea6shipping.py
+++ Tarea6shipping.py
@@ -7,4 +7,5 @@ else:
     shipping_cost = customer_basket_weight * shipping_cost_per_kg
     customer_cost = shipping_cost + customer_basket_cost
 
-print ("total basket cost incluidin shipping is" + str(customer_cost))  
+print ("total basket cost incluidin shipping is" + str(customer_cost))
+# https://www.figma.com/file/w9fhJf3WYddTLNtUtjPfcY/primer-diagrama?node-id=0%3A1
